a37e4b8c84b51340c8b6c46df103b9ab36fa2d1e
Sbencee <99654274+Sbencee@users.noreply.github.com>
2025-11-06 21:02:27 +0100

    Delete Proba directory

--- Proba/src/composite/LibraryComponent.java
+++
@@ -1,7 +0,0 @@
-// src/main/java/com/example/library/composite/LibraryComponent.java
-package com.example.library.composite;
-
-import java.util.List;
-
-public interface LibraryComponent {
-    void display(String indent);
